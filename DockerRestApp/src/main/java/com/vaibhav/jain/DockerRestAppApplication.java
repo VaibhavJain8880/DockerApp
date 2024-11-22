6778e3bc5a740b08fe4363ac4b67d92c1e0ffadc
Vaibhav Jain <vaibhavjain8880@gmail.com>
2024-11-22 12:19:58 +0530

    boot rest

---
+++ DockerRestApp/src/main/java/com/vaibhav/jain/DockerRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.jain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerRestAppApplication.class, args);
+	}
+
+}
